cd8aa3fcdddf82fda44fb956f4b7a10e55009f65
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-04 17:25:26 +0530

    C1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* <LinearGradient> */}
+          {/* <AuroraBackground> */}
+          {children}
+          {/* </AuroraBackground> */}
+          {/* </LinearGradient> */}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
